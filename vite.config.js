a40f65081dc8bd8bce028c64c07ba3ce10d6c077
Manuel <gtr2500@gmail.com>
2025-08-26 17:30:22 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  base: '/BudgetBox/',
+  base: '/BudgetBox/',   // 👈 per GitHub Pages repo "BudgetBox"
 })
